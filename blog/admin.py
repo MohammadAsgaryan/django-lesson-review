369e3021e6f72b040a4369c359cceceabca425e9
mohamad asgaryan shad <mohmadasgaryanshad@email.com>
2026-04-20 00:38:03 +0330

    Admin Panel Customization
    add:
    date_hierarchy, empty_value_display, list_display, list_filter, search_fields
    in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'published_date'
+    empty_value_display = '-empty-'
+    list_display = ('title' ,'counted_view','status','published_date','created_date')
+    list_filter = ['status']
+    search_fields = ['title','content']
 
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
