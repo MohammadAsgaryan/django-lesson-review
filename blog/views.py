aeccdf4109b38c56cb9219bec40ed72bac8bb59e
mohamad asgaryan shad <mohmadasgaryanshad@email.com>
2026-04-24 17:11:13 +0330

    Add previous/next post navigation with dynamic links and image sizing
    +Implemented logic in `blog_single` view to fetch previous and next posts
    +Added conditional rendering for prev/next post sections in the template
    +Linked navigation arrows, titles, and thumbnails to the correct post URLs
    +Applied custom CSS class to control thumbnail image size using `object-fit: cover`
    + Improved template structure to prevent empty UI blocks when a post is missing

--- blog/views.py
+++ blog/views.py
@@ -10,5 +10,20 @@ def blog_view(request):
 
 def blog_single(request,pid):
     post = get_object_or_404(Post,pk=pid,status=1)
-    context = {'post': post}
+    posts = Post.objects.filter(status=1).order_by('created_date')
+    posts_list = list(posts)
+    current_index = posts_list.index(post)
+    prev_post = None
+    next_post = None
+    if current_index > 0:
+        prev_post = posts_list[current_index - 1]
+
+    # اگر پست بعدی وجود دارد
+    if current_index < len(posts_list) - 1:
+        next_post = posts_list[current_index + 1]
+    context = {
+        'post': post,
+        'prev_post': prev_post,
+        'next_post': next_post,
+    }
     return render(request, 'blog/blog-single.html',context)
