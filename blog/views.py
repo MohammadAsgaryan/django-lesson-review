d247b8634c3aeba7003cad807d0e4afa9da86d7f
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-04-26 15:49:07 +0330

    added category and user filter
    changes pages and added some features
    pagination
    template tags
    and so

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .models import Post
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(status=1)
-    context = {'posts' : posts}
-    return render(request, 'blog/blog-home.html', context)
-
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
     post = get_object_or_404(Post,pk=pid,status=1)
@@ -27,3 +39,11 @@ def blog_single(request,pid):
         'next_post': next_post,
     }
     return render(request, 'blog/blog-single.html',context)
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
+    
+    
