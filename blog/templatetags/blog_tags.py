6682a1c58189c839c0dc1371213eebce84caeb2a
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-04-26 16:53:30 +0330

    Add custom template tag to display latest 6 posts on homepage
    + Created a reusable Django template tag for fetching the latest 6 posts
    + Integrated the tag into the homepage "latest posts" section
    + Improved homepage structure by separating data logic from template

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -16,3 +16,8 @@ def postcategories():
     for name in categories:
         cat_dict[name]=posts.filter(category=name).count()
     return {'categories':cat_dict}
+
+@register.inclusion_tag('blog/recent-blog.html')
+def recentblog(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
+    return {'posts':posts}
