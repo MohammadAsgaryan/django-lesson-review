b5aa2e1e0fe8480c9ae2a8b76f3a7ea30737a5f8
mohamad asgaryan shad <mohamadasgaryanshad@gmail.com>
2026-04-30 16:17:14 +0330

    added captcha  and captcha admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,8 @@ INSTALLED_APPS = [
     'robots',
     'taggit',
     'django_summernote',
+    'captcha',
+    
 ]
 
 #site framework
@@ -85,6 +88,13 @@ SUMMERNOTE_CONFIG = {
         ],
     }
 }
+
+
+# captcha admin settings
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'simple-captcha',
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- website/forms.py
+++ website/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from .models import Contact,Newslatter
+from captcha.fields import CaptchaField
 
 class ContactModelform(forms.ModelForm):
+    captcha = CaptchaField()
     class Meta:
         model = Contact
         fields ='__all__'
+        
         
 
 class NewslatterForm(forms.ModelForm):
